b46264ed09160f0cf6f7758f32685fff08e80158
danialevil <32008458+danialevil@users.noreply.github.com>
2022-09-25 02:46:53 +0330

    Add files via upload

---
+++ Assets/Script/LeadLevel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeadLevel : MonoBehaviour
+{
+    public void Load()
+    {
+        Application.LoadLevel("Game");
+    }
+}
